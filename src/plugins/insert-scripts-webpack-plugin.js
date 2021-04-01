bb4ca0b33131bb3e050adc8a49ca2884dac0181c
Cavendichun <cavendichun@163.com>
2021-04-01 14:08:26 +0800

    feat: scripts属性增加对象配置模式，支持defer,async等参数
    ✅ Closes: #43

--- src/plugins/insert-scripts-webpack-plugin.js
+++ src/plugins/insert-scripts-webpack-plugin.js
@@ -10,18 +10,47 @@ class InsertScriptsWebpackPlugin {
     compiler.hooks.compilation.tap(
       "InsertScriptsWebpackPlugin",
       compilation => {
-        HtmlWebpackPlugin.getHooks(
-          compilation
-        ).beforeAssetTagGeneration.tapAsync(
+        HtmlWebpackPlugin.getHooks(compilation).afterTemplateExecution.tapAsync(
           "InsertScriptsWebpackPlugin",
           (data, callback) => {
             if (Array.isArray(this.options.scripts)) {
-              // 是数组，说明是单页面应用或多页面共用一套scripts
-              data.assets.js = [...this.options.scripts, ...data.assets.js];
+              /**
+               * 是数组，说明是单页面应用或多页面共用一套scripts
+               * 接下来判断每一个item是否是字符串形式
+               * 如果是，直接插入script.src
+               * 否则，排除src，其他属性作为attributes
+               */
+              const appendScripts = [];
+              this.options.scripts.forEach(item => {
+                appendScripts.push({
+                  tagName: "script",
+                  voidTag: false,
+                  attributes:
+                    typeof item === "string"
+                      ? {
+                          src: item,
+                        }
+                      : item,
+                });
+              });
+              data.bodyTags = [...appendScripts, ...data.bodyTags];
             } else {
               const key = data.outputName.replace(/\.html$/, "");
               const tar = this.options.scripts[key] || [];
-              data.assets.js = [...tar, ...data.assets.js];
+              const appendScripts = [];
+              tar.forEach(item => {
+                appendScripts.push({
+                  tagName: "script",
+                  voidTag: false,
+                  attributes:
+                    typeof item === "string"
+                      ? {
+                          src: item,
+                        }
+                      : item,
+                });
+              });
+              data.bodyTags = [...appendScripts, ...data.bodyTags];
             }
             callback(null, data);
           }
